b8b47665d969198296f2b3ede92b820e1b373290
adupoku71 <davidpoku71@gmail.com>
2025-08-23 10:10:20 +0000

    added general book model

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 
+
 # Create your models here.
 class Genre(models.Model):
     """Model representing a book genre"""
@@ -22,3 +23,26 @@ class Genre(models.Model):
                 violation_error_message = "Genre already exists (case insensitive match)",
             )
         ]
+        
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+
+    genre = models.ManyToManyField(
+        Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
