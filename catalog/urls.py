dcd0a49fbe2a35bda2b5ae75563dbb75ba50d727
adupoku71 <davidpoku71@gmail.com>
2025-09-20 22:38:20 +0000

    implement authors list page and author details

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
